620e065c0bc720d11f01fbdef41b80eaf9d59dc3
thooijenga <thooijenga@snelstart.nl>
2021-01-15 15:32:17 +0100

    Add linting with eslint

---
+++ eslint/src.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "./style.js"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "plugins": ["@typescript-eslint"],
+};
---
+++ eslint/test.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "es2021": true,
+        "mocha": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "./style.js"
+    ],
+};
