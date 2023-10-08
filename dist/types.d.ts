e1382f3853d4b49a9cedf38663732bb00313c7c6
Tom <tomhooijenga@gmail.com>
2023-10-08 19:05:18 +0200

    Build

--- dist/types.d.ts
+++ dist/types.d.ts
@@ -1,8 +1,8 @@
 import { Queue } from "./queue";
-export declare type ExecuteBatch<Result, Args> = (args: Args[]) => Result[] | Promise<Result[]>;
-export declare type Scheduler = (queue: unknown[][], flush: () => void) => void;
-export declare type Resolve<Result> = (value: Result | PromiseLike<Result>) => void;
-export declare type Reject = (reason?: unknown) => void;
+export type ExecuteBatch<Result, Args> = (args: Args[]) => Result[] | Promise<Result[]>;
+export type Scheduler = (queue: unknown[][], flush: () => void) => void;
+export type Resolve<Result> = (value: Result | PromiseLike<Result>) => void;
+export type Reject = (reason?: unknown) => void;
 export interface AddToBatch<Result, Args extends unknown[]> {
     (...args: Args): Promise<Result>;
     queue: Queue<Result, Args>;
